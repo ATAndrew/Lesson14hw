88db46b6a2def51ecac21fb701b969c4ecebe480
Tirsu Andrew <tirsu.andrew@gmail.com>
2020-04-27 21:21:01 +0300

    JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/*
+let availableQuantity = 10;
+let answer = prompt("Ordered Quantity", );
+
+if( answer > availableQuantity) 
+    console.log ("Sorry, we don’t have enough in stock");
+
+else 
+
+    console.log ("Great, we will soon ship your order");
+
+*/
+
+let answer = prompt("Ordered Quantity")
+let availableQuantity = answer > 10 ? console.log ("Sorry we don't have enough in stock") : console.log ("Great we will soon ship your order") ;
+
+
